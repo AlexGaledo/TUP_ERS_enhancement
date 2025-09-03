cde4bf38e0e3ec270cb83d31b133ef0e339be1a4
SPlinterRed <131634018+SPlinterRed@users.noreply.github.com>
2025-09-03 23:11:21 +0800

    initialized config

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,10 @@
-
-
-from flask import Flask
-from config import Config
+from flask import Flask, jsonify
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
+from .config import Config
 
 
-
+#initialize app
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
--- app/config.py
+++ app/config.py
@@ -3,6 +3,7 @@ import os
 
 load_dotenv()
 
+# mga susi
 class Config():
     SQLALCHEMY_DATABASE_URI = os.getenv('sql_url')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
