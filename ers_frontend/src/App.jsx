4b3486ce45f4a4e1fa8d8f69c92c6c6388a883f7
Ford Torion <clifford.torion04@gmail.com>
2025-10-22 10:00:55 +0800

    header component

--- ers_frontend/src/App.jsx
+++ ers_frontend/src/App.jsx
@@ -6,15 +6,17 @@ import Auth from './pages/auth.jsx'
 import ResetPage from './pages/forgetpass.jsx'
 import ChangePass from './pages/changepass.jsx'
 import Otp from './pages/otp.jsx'
-import { useNavigate } from 'react-router-dom'
 
+import Home from './pages/Home.jsx';
+
+import { useNavigate } from 'react-router-dom'
 
 function App() {  
   const navigate = useNavigate();
   return (
     <>
     <Routes>
-      <Route path='/' element={<Auth/>}/>
+      <Route path='/' element={<Home/>}/>
       <Route path='/auth' element={<Auth/>}/>
       <Route path='/forget-password' element={<ResetPage/>}/>
       <Route path='/reset-password/:token' element={<ChangePass/>}/>
---
+++ ers_frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import "../css/Home.css";
+import Header from "../components/Header";
+
+export default function Home() {
+    return (
+        <div className="home-container">
+            <Header />
+            <h1>HOME</h1>
+        </div>
+    )
+}
